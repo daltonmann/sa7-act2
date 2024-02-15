a3bb146c36942a453f67f491ff6c410ecf5be7f1
Dalton Mann <daltonjamann@gmail.com>
2024-02-15 12:24:38 -0600

    Completed problem 6

---
+++ prob6.rb
@@ -0,0 +1,7 @@
+
+
+student = {"name"=>"Alice", "age"=>20, "grade"=> "A"}
+
+#student = {name: "Alice", age: 20, grade: "A"}
+
+puts "#{student["name"]} is #{student["age"]} years old and has an #{student["grade"]} in the class"
